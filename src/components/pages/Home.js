1641ff191824f26b7139ac985c97cc0098709453
Abadaike Collins <abadaikecollins@gmail.com>
2021-02-06 12:36:32 +0100

    making updates

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,9 +3,6 @@ import Navbar from '../layout/Navbar';
 import Data from '../data/Data';
 import Filter from '../filter/Filter';
 import ContactContext from '../../context/data/dataContext';
-import 'jquery';
-import 'datatables.net-dt/js/dataTables.dataTables';
-import 'datatables.net-dt/css/jquery.dataTables.min.css';
 
 const Home = () => {
 	const contactContext = useContext(ContactContext);
--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 const NotFound = () => {
-    return (
-        <div>
-            <h1>404</h1>
-            <h2>Page Not Found</h2>
-            <h6>The page you are looking does not exit...</h6>
-        </div>
-    )
-}
+	return (
+		<div>
+			<nav className="blue">
+				<div class="nav-wrapper container">
+					<a href="#" class="brand-logo">
+						Json Handler
+					</a>
+					<ul id="nav-mobile" class="right hide-on-med-and-down">
+						<li>
+							<a href="/">Go Home</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			<div className="container">
+				<h1>404</h1>
+				<h2>Looks like you are lost</h2>
+				<a href="/" className="waves-effect waves-light btn-large blue">
+					Go home
+				</a>
+			</div>
+		</div>
+	);
+};
 export default NotFound;
